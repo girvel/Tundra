df3f6c9a4a922d75a648d34ee31bfd0770293a12
girvel <widauka@ya.ru>
2019-08-21 18:30:06 +0500

    Try to create built distribution

---
+++ setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(
+    name="Tundra",
+    version="0.1",
+    author="girvel",
+    author_email="widauka@ya.ru",
+    description="Simple text quest",
+    url="https://github.com/girvel/Tundra",
+)
